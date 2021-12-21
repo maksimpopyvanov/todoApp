a7d8d3230d0c40b0e800052c0cc8e3e578916825
Maksim Popyvanov <maksimpopyvanov@gmail.com>
2021-12-21 17:01:53 +0300

    init schema

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,27 @@ import (
 	"log"
 	todo "todo"
 	handler "todo/pkg/handler"
+	repository "todo/pkg/repository"
+	service "todo/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
-		log.Printf("error ocured while running http.server: %s", err.Error())
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
 	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error ocured while running http.server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
