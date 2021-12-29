373b88f4a797a0b94349dc70e3e8da2c9abb422e
Maksim Popyvanov <maksimpopyvanov@gmail.com>
2021-12-29 14:22:15 +0300

    add primary key

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id            serial       not null unique,
+    id            serial primary key,
     name          varchar(255) not null,
     username      varchar(255) not null unique,
     password_hash varchar(255) not null
@@ -8,21 +8,21 @@ CREATE TABLE users
 
 CREATE TABLE todo_lists
 (
-    id          serial       not null unique,
+    id          serial       primary key,
     title       varchar(255) not null,
     description varchar(255)
 );
 
 CREATE TABLE users_lists
 (
-    id      serial                                           not null unique,
+    id      serial                                           primary key,
     user_id int references users (id) on delete cascade      not null,
     list_id int references todo_lists (id) on delete cascade not null
 );
 
 CREATE TABLE todo_items
 (
-    id          serial       not null unique,
+    id          serial       primary key,
     title       varchar(255) not null,
     description varchar(255),
     done        boolean      not null default false
@@ -31,7 +31,7 @@ CREATE TABLE todo_items
 
 CREATE TABLE lists_items
 (
-    id      serial                                           not null unique,
+    id      serial                                           primary key,
     item_id int references todo_items (id) on delete cascade not null,
     list_id int references todo_lists (id) on delete cascade not null
 );
